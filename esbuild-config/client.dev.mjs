0b30eb00b5ef0f9d3438894ad50dcaff01054a8c
SangMinL96 <hwon3794@gmail.com>
2023-12-08 23:41:34 +0900

    .

--- esbuild-config/client.dev.mjs
+++ esbuild-config/client.dev.mjs
@@ -14,7 +14,7 @@ try {
 	ctx = await esbuild.context({
 		entryPoints: ['src/index.jsx'],
 		bundle: true,
-		minify: false,
+		minify: true,
 		format: "cjs",
 		
 		loader: { '.svg': 'text', ".module.scss": "local-css", ".png": "dataurl" },
