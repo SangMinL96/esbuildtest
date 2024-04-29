89676162353ad7b827496b2cd5f6c0c6fd27fc11
SangMinL96 <hwon3794@gmail.com>
2024-04-29 22:57:27 +0900

    .

--- esbuild-config/server.dev.mjs
+++ esbuild-config/server.dev.mjs
@@ -7,7 +7,7 @@ try {
 		entryPoints: ['src/server/server.ts'],
 		bundle: true,
 		sourcemap: false,
-		minify: true,
+		minify: false,
 		platform: 'node',
 		target: ['node18.6'],
 		packages: 'external',
